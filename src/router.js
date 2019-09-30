7a6918cc99bbee2e72d5a5ece595a3ec0f2b3ff1
zhaoyuchen886 <24588613732qq.com>
2019-09-30 18:48:13 +0800

    新建登录页和主页

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/'
+import Home from './views/home/' // 引入主页
+import Login from './views/login' // 引入登录页
 
 Vue.use(Router)
 
@@ -8,7 +9,15 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
