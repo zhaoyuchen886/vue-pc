a98900e17c6a682720ae2ab1c560fc38d006ec7a
zhaoyuchen886 <24588613732qq.com>
2019-09-30 18:19:49 +0800

    安装element,整理目录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element-ui,并全局注册
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入element-UI样式
+// 映入项目初始样式
+import './styles/index.less'
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
